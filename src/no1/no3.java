0a15a9a344aa1a62e85204a921fe2c275831ca78
HP <HP@DESKTOP-2RRC89C>
2022-04-13 11:09:52 +0700

    upload project

---
+++ src/no1/no3.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package no1;
+import java.util.Scanner;
+/**
+ *
+ * @author HP
+ */
+public class no3 {
+ 
+
+    public static void main(String[] args) {
+        Scanner pengurangan = new Scanner (System.in);
+int Pertama, Kedua, Ketiga, Keempat, HasilJumlah;
+System.out.print("1. Masukan Angka : ");
+    Pertama = pengurangan.nextInt();
+System.out.print("2. Masukan Angka : ");
+    Kedua = pengurangan.nextInt();
+System.out.print("3. Masukan Angka : ");
+    Ketiga = pengurangan.nextInt();
+System.out.print("4. Masukan Angka : ");
+    Keempat = pengurangan.nextInt();    HasilJumlah = Pertama + Kedua + Ketiga + Keempat;
+System.out.println("Hasil Penjumlahan dari "+Pertama+" - "+Kedua+" - "+Ketiga+" - "+Keempat+" = "+HasilJumlah);
+  System.out.println("mulyanamoelk.blogspot.com"); 
+    }
+
+
+}
+
